2e334e5418af31058b8e90846756b194f93fa9fa
marsjava <marikannan.cvp@gmail.com>
2020-09-23 17:02:26 +0530

    springboot initial commit

---
+++ todo-springboot-stack/src/main/java/com/todo/stack/TodoSpringbootStackApplication.java
@@ -0,0 +1,13 @@
+package com.todo.stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringbootStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringbootStackApplication.class, args);
+	}
+
+}
---
+++ todo-springboot-stack/src/test/java/com/todo/stack/TodoSpringbootStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringbootStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
